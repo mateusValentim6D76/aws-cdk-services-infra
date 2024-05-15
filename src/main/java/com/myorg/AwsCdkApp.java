b0ba10ffe753fd95b082ae21b5933c1d1a95f15d
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2024-05-14 23:15:24 -0300

    Criando VPC via CDK

---
+++ src/main/java/com/myorg/AwsCdkApp.java
@@ -0,0 +1,16 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+import java.util.Arrays;
+
+public class AwsCdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+        new VpcStack(app, "Vpc01");
+        app.synth();
+    }
+}
+
