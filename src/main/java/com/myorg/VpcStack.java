1570965424dc9f78775e738a9f0a358567cc91b6
Mateus Valentim <38231569+mateusValentim6D76@users.noreply.github.com>
2024-07-29 23:40:37 -0300

    Update VpcStack.java

--- src/main/java/com/myorg/VpcStack.java
+++ src/main/java/com/myorg/VpcStack.java
@@ -1,19 +1,22 @@
 package com.myorg;
 
+
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.constructs.Construct;
 
 public class VpcStack extends Stack {
-    private Vpc vpc;
+
+    private final Vpc vpc;
+
     public VpcStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
     public VpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
-        Vpc.Builder.create(this, "Vpc01")
+        this.vpc = Vpc.Builder.create(this, "Vpc01")
                 .natGateways(0)
                 .maxAzs(2)
                 .build();
