dbbe81fdcb60374a9f6abde4991f60e031e91675
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2024-08-18 20:23:16 -0300

    Atribuindo a permissao a tarefa do ECS para acessar a fila atraves do CDK

--- src/main/java/com/myorg/Service02Stack.java
+++ src/main/java/com/myorg/Service02Stack.java
@@ -96,5 +96,7 @@ public class Service02Stack extends Stack {
                 .scaleInCooldown(Duration.seconds(60))
                 .scaleOutCooldown(Duration.seconds(60))
                 .build());
+
+        productEventsQueue.grantConsumeMessages(productServiceConsumer.getTaskDefinition().getTaskRole());
     }
 }
